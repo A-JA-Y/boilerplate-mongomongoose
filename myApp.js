280b448b41b27a9e4dbbb8f0d684d9b6170a7b3e
Ajay <mailtoajay7007@gmail.com>
2024-12-05 23:13:53 +0530

    mongo uri cnnection

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
 let Person;
 
